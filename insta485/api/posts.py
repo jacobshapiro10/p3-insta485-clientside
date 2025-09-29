5bab3db03b4bb8facf99ad7e5ccc9ffcf4134f78
Jacob Shapiro <jacobshap518@gmail.com>
2025-09-29 15:12:12 -0400

    started with authorization

--- insta485/api/posts.py
+++ insta485/api/posts.py
@@ -1,6 +1,7 @@
 """REST API for posts."""
 import flask
 import insta485
+import hashlib
 
 
 
@@ -14,33 +15,58 @@ def get_api_root():
 }
   return flask.jsonify(**context)
 
+"""
+@insta485.app.route('/api/v1/posts/')
+def show_posts():
+
+"""
   
 
 
 @insta485.app.route('/api/v1/posts/<int:postid_url_slug>/')
 def get_post(postid_url_slug):
-    """Return post on postid.
+  auth = flask.request.authorization
 
-    Example:
-    {
+  if auth is None:
+      return flask.jsonify({
+          "message": "Forbidden",
+          "status_code": 403
+      }), 403
+
+  username = auth.username
+  password = auth.password
+
+  connection = insta485.model.get_db()
+  user = connection.execute(
+    "SELECT password FROM users WHERE username = ?", (username,)
+  ).fetchone()
+
+  if user is None:
+    return flask.jsonify({
+          "message": "Forbidden",
+          "status_code": 403
+      }), 403
+
+  algorithm, salt, stored_hash = user["password"].split("$")
+
+  hash_obj = hashlib.new(algorithm)
+  hash_obj.update((salt + password).encode("utf-8"))
+  computed_hash = hash_obj.hexdigest()
+
+  if computed_hash != stored_hash:
+    return flask.jsonify({
+          "message": "Forbidden",
+          "status_code": 403
+      }), 403
+   
+  context = {
       "created": "2017-09-28 04:33:28",
       "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
       "owner": "awdeorio",
       "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
       "ownerShowUrl": "/users/awdeorio/",
-      "postShowUrl": "/posts/1/",
-      "postid": 1,
-      "url": "/api/v1/posts/1/"
-    }
-    """
-    context = {
-        "created": "2017-09-28 04:33:28",
-        "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
-        "owner": "awdeorio",
-        "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
-        "ownerShowUrl": "/users/awdeorio/",
-        "postShowUrl": f"/posts/{postid_url_slug}/",
-        "postid": postid_url_slug,
-        "url": flask.request.path,
-    }
-    return flask.jsonify(**context)
+      "postShowUrl": f"/posts/{postid_url_slug}/",
+      "postid": postid_url_slug,
+      "url": flask.request.path,
+  }
+  return flask.jsonify(**context)
